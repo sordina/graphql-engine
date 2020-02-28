d286433ba04c01fe1222f8ba0e2c4eafca0f0527
Lyndon Maydwell <lyndon@sordina.net>
2020-02-28 13:45:51 +0530

    cloud benchmark suite

---
+++ cloud/load-testing/spawn-rds.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+N="${1:-1}"
+
+T="$(date +%s)"
+
+echo "spawning $N rds instances"
+
+for I in $(seq 1 "$N")
+do
+
+	echo "spawning instance $I"
+
+	IID="hasura-cloud-benchmark-V2-$I"
+
+	aws --profile=hasura --region=ap-south-1 rds create-db-instance            \
+		--db-instance-identifier "$IID"                                          \
+		--engine postgres                                                        \
+		--tags Key=purpose,Value=hasura-cloud-benchmark                          \
+		--db-instance-class db.m5.xlarge                                         \
+		--allocated-storage 20                                                   \
+		--master-username master                                                 \
+		--master-user-password "$DB_PASSWORD"                                    \
+		--publicly-accessible                                                    \
+		| tee "logs/aws_rds_logs_${T}_${I}.json"
+
+	aws --profile=hasura \
+		--region=ap-south-1 rds describe-db-instances \
+		--db-instance-identifier="$IID" \
+		| jq -r '.DBInstances | .[] | .Endpoint.Address' \
+		> "logs/aws_rds_logs_${T}_${I}_connection_string.txt"
+
+done
